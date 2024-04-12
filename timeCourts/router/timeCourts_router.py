f8006a31bc5a4dd442218aa1098c83d55aa301ff
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-04-11 22:54:58 -0500

    (Feat) Searh endpoint courts by date and location

--- timeCourts/router/timeCourts_router.py
+++ timeCourts/router/timeCourts_router.py
@@ -9,3 +9,7 @@ router = APIRouter()
 @router.get("/timeCourts/{location}", response_model=response.APIResponse)
 def read_timeCourts(location:str):
     return timeCourts_infrastructure.get_timeCourts(location)
+
+@router.get("/timeCourts/{day}/{month}/{year}/{location}", response_model=response.APIResponse)
+def read_timeCourts_by_date(day:str, month:str, year:str, location:str):
+    return timeCourts_infrastructure.get_timeCourts_by_date(day, month, year, location)
