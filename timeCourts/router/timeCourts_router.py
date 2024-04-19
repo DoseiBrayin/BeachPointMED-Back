4a38697d7c72a8d4faab23e4c96348d08d444eff
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-04-19 10:11:39 -0500

    (ADD) Adding dependecies to use HTTP bearer token

--- timeCourts/router/timeCourts_router.py
+++ timeCourts/router/timeCourts_router.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 from fastapi.encoders import jsonable_encoder
 from models import response
 from timeCourts.infrastructure import timeCourts_infrastructure
+from JWT.JWTBearer import JWTBearer
 
 router = APIRouter()
 
 # Aquí estamos definiendo las rutas de nuestra API REST
-@router.get("/timeCourts/{location}", response_model=response.APIResponse)
+@router.get("/timeCourts/{location}", response_model=response.APIResponse, dependencies=[Depends(JWTBearer())])
 def read_timeCourts(location:str):
     return timeCourts_infrastructure.get_timeCourts(location)
 
-@router.get("/timeCourts/{date}/{location}", response_model=response.APIResponse)
+@router.get("/timeCourts/{date}/{location}", response_model=response.APIResponse, dependencies=[Depends(JWTBearer())])
 def read_timeCourts_by_date(date:str,location:str):
     return timeCourts_infrastructure.get_timeCourts_by_date(date,location)
