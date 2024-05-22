906b3fd7627e1b2e1c0391d1df21237baa785e4b
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-22 09:55:37 -0500

    WIP Fixing payment endpoint

--- payment/router/paymente_router.py
+++ payment/router/paymente_router.py
@@ -2,12 +2,12 @@ from models import response
 from JWT.JWTBearer import JWTBearer
 from payment.infrastructure import payment_infra
 from payment.models.payment_model import PaymentForm
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
-@router.post("/")
-async def payment(request: PaymentForm):
+@router.post("/{PaymentForm}")
+async def payment(request: PaymentForm = Depends()):
     return await payment_infra.payment(request)
 
