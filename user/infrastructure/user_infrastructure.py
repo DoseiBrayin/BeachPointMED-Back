0c2bf11716ed7fc37ef0dd1fc717bfe56e351f9b
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-06-11 15:52:42 -0500

    (Refactoring) Refactoring user endpoints

--- user/infrastructure/user_infrastructure.py
+++ user/infrastructure/user_infrastructure.py
@@ -1,6 +1,7 @@
 from db.connection import Session
 from models import response
-from user.model import user_model, user_response
+from user.model import user_response
+from db.models.BPDataBase import Users
 from fastapi import HTTPException
 from sqlalchemy.exc import IntegrityError
 from JWT.jwtmanager import create_token
@@ -8,7 +9,7 @@ from JWT.jwtmanager import create_token
 
 def create_user(user_data: user_response.UserResponse):
     try:
-        user = user_model.User(**user_data.dict())
+        user = Users(**user_data.dict())
         session = Session()
         session.add(user)
         session.commit()
@@ -28,8 +29,8 @@ def create_user(user_data: user_response.UserResponse):
 
 def login(user: user_response.LoginResponse):
     session = Session()
-    user = session.query(user_model.User).filter(
-        (user_model.User.email == user.email) & (user_model.User.password == user.password)).first()
+    user = session.query(Users).filter(
+        (Users.email == user.email) & (Users.password == user.password)).first()
     session.close()
     if user is None:
         raise HTTPException(status_code=404,
@@ -40,7 +41,7 @@ def login(user: user_response.LoginResponse):
 def get_user():
     try:
         session = Session()
-        users = session.query(user_model.User).all()
+        users = session.query(Users).all()
         session.close()
         users_list = [{key: value for key, value in user.__dict__.items() if key != '_sa_instance_state'} for user in users]
         return response.APIResponse(data=users_list, status="success", message="Users have been successfully retrieved")
